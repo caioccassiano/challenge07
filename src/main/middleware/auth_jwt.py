7d59df40db9b430f366eb8795abb6c65648daa24
caioccassiano <caioccassiano@icloud.com>
2025-06-01 17:03:26 +0200

    feat: Implenting list orders route

--- src/main/middleware/auth_jwt.py
+++ src/main/middleware/auth_jwt.py
@@ -8,15 +8,17 @@ def auth_jwt_verify():
 
   if not raw_token or not user_id:
     raise Exception("Invalid Auth informations")
+  try:
+    token = raw_token.split()[1] #We gotta do this because it's a Bearer token and tje [0] is gonna be "Bearer"
+    token_informations = jwt_handler.decode_jwt_token(token) # We call the function to decode the token after extract it from raw_token
+    token_uid = token_informations["user_id"]
+    
+    if user_id and token_uid and (int(token_uid)== int(user_id)):
+      return token_informations
+  except Exception:
+    raise Exception("Invalid or expired token")
   
-  token = raw_token.split()[1] #We gotta do this because it's a Bearer token and tje [0] is gonna be "Bearer"
-  token_informations = jwt_handler.decode_jwt_token(token) # We call the function to decode the token after extract it from raw_token
-  token_uid = token_informations["user_id"] # We take the user_id so we can pass it as a parameter
 
-  if user_id and token_uid and (int(token_uid)== int(user_id)):
-    return token_informations
   
-  raise Exception("User Unauthorized")
-                           
 
   
